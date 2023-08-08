22771110a224d0ee19fdef8322e7ffd3230638db
Audrey Cheng <audreycc@fb.com>
2023-08-07 18:48:33 -0700

    defer

--- storage/innobase/trx/trx0sys.cc
+++ storage/innobase/trx/trx0sys.cc
@@ -768,7 +768,7 @@ void trx_sys_create(void) {
   }
 
   /* TODO(accheng): number of clusters is currently hardcoded. */
-  trx_sys->num_clusters = 256;//32; //32; //4; //
+  trx_sys->num_clusters = 32; //32; //4; //
 
   /* TODO(accheng): number of hot keys is currently hardcoded. */
   trx_sys->num_hot_keys = 4; // 4;
